b2b10f54bd3b05e231ef34e9fde72a6d7a6d4d6a
Agung Adhinata <adhi0asta@gmail.com>
2024-06-30 09:45:13 +0800

    feat: use typescript

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -2,14 +2,17 @@ import Home from "./pages/index";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Laporan from "./pages/laporan";
 import WebSocketDemo from "./pages/index";
+import MainLayout from "./layout/MainLayout";
 
 function App() {
   return (
     <Router>
       <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/laporan" element={<Laporan />} />
-        <Route path="/websoket" element={<WebSocketDemo />} />
+        <Route path="/" element={<MainLayout />}>
+          <Route index element={<Home />} />
+          <Route path="laporan" element={<Laporan />} />
+          <Route path="websoket" element={<WebSocketDemo />} />
+        </Route>
       </Routes>
     </Router>
   );
