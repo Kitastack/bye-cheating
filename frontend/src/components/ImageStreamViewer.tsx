d93b9390c925b745675d69b60389a6243189bcd1
Agung Adhinata <adhi0asta@gmail.com>
2024-06-30 10:20:06 +0800

    feat: cleanup UI

--- frontend/src/components/ImageStreamViewer.tsx
+++ frontend/src/components/ImageStreamViewer.tsx
@@ -1,11 +1,10 @@
 import { useEffect, useState } from "react";
 
 const getBlobUrl = (data: any) => {
-    if(data) {
-        return `data:image/jpeg;base64, ${data}`;
-    }
-    return
-  
+  if (data) {
+    return `data:image/jpeg;base64, ${data}`;
+  }
+  return;
 };
 
 export function ImageStreamViewer({
@@ -18,17 +17,19 @@ export function ImageStreamViewer({
   );
   useEffect(() => {
     setBase64String(getBlobUrl(socketEvent?.data));
-    console.log(`sizedata: ${base64String}`)
+    console.log(`sizedata: ${base64String}`);
   }, [socketEvent]);
   return (
-    <div className="border-4 border-solid rounded-lg flex place-content-center mt-5 px-10">
-      {base64String ? (
-        <img src={base64String} alt="CCTV Stream" />
-      ) : (
-        <h1 className="place-content-center text-2xl bg-slate-400 mx-auto my-auto px-7 py-2 rounded-lg bg-opacity-30">
-          Tidak Ada Video CCTV
-        </h1>
-      )}
+    <div className="flex justify-center">
+      <div className="border-4 border-solid min-w-[640px] aspect-video rounded-lg flex place-content-center mt-5 px-10">
+        {base64String ? (
+          <img src={base64String} alt="CCTV Stream" />
+        ) : (
+          <h1 className="place-content-center text-2xl bg-slate-400 mx-auto my-auto px-7 py-2 rounded-lg bg-opacity-30">
+            Tidak Ada Video CCTV
+          </h1>
+        )}
+      </div>
     </div>
   );
 }
