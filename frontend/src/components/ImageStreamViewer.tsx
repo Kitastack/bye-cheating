74fb7d02da7a8a0ced63d0c2338978cc9580d8d2
Agung Adhinata <adhi0asta@gmail.com>
2024-08-02 07:34:09 +0800

    feat: reducte tailwind usage

--- frontend/src/components/ImageStreamViewer.tsx
+++ frontend/src/components/ImageStreamViewer.tsx
@@ -12,14 +12,12 @@ import { MdOutlineWarningAmber } from "react-icons/md";
 export function ImageStreamViewer({
   base64Data,
   error,
-  minW,
 }: {
   /**
    * Base64 image string, not include `data:image/jpeg;base64,`
    */
   base64Data?: string;
   error?: string;
-  minW?: number;
 }) {
   const [base64String, setBase64String] = useState<string | undefined>(
     undefined
@@ -31,8 +29,7 @@ export function ImageStreamViewer({
   return (
     <Box
       bd={"1px solid myColor.5"}
-      style={{ minWidth: minW ?? 600, minHeight: ((minW ?? 600) * 9) / 16 }}
-      className={`resize overflow-auto aspect-video rounded-sm flex justify-center items-center`}
+      className={`overflow-auto aspect-video w-full rounded-sm flex justify-center items-center`}
     >
       {base64String ? (
         <img src={base64String} alt="CCTV Stream" />
--- frontend/src/pages/app/livecam.jsx
+++ frontend/src/pages/app/livecam.jsx
@@ -1,20 +1,22 @@
 import { ImageStreamViewer } from "@/components/ImageStreamViewer";
 import { useStreamSocket } from "@/context/CameraSocketContext";
-import { MdCamera, MdOutlinePlayArrow, MdTableView } from "react-icons/md";
-import { ActionIcon, Table, Text, TextInput, Tooltip } from "@mantine/core";
+import { MdCamera, MdTableView } from "react-icons/md";
+import {
+  ActionIcon,
+  Card,
+  Flex,
+  Table,
+  Tooltip,
+} from "@mantine/core";
 import { useState } from "react";
 import { ReportTable } from "@/components/ReportTable";
 import { sampleDataCCTV } from "@/model/dataset";
 
 const CameraView = () => {
   return (
-    <div className="flex flex-grow p-4">
-      <div className="flex-grow flex flex-col items-center">
+    <Flex direction={"column"} gap={"xs"} p={"xs"}>
         <MainView />
-      </div>
-      <div className="border-l border-gray-300 dark:border-gray-300/30 min-h-full"></div>
-      <Inspector />
-    </div>
+    </Flex>
   );
 };
 
@@ -23,71 +25,47 @@ const MainView = () => {
   const [showTable, setShowTable] = useState(false);
   return (
     <>
-      <span className="flex w-full justify-between pr-4">
+      <Flex justify={"space-between"} w={"100%"}>
         <span className="flex flex-col">
           <h1 className="text-xl font-bold">Nama Camera</h1>
           <h3>Tanggal: 19 Februari 2024</h3>
         </span>
-        <ActionIcon.Group>
-          <Tooltip label="Tampilan Kamera">
-            <ActionIcon
-              variant={showTable ? "default" : "filled"}
-              size={"lg"}
-              onClick={() => setShowTable(false)}
-              aria-label="Camera"
-            >
-              <MdCamera />
-            </ActionIcon>
-          </Tooltip>
-          <Tooltip label="Tampilan Tabel">
-            <ActionIcon
-              variant={showTable ? "filled" : "default"}
-              size={"lg"}
-              onClick={() => setShowTable(true)}
-              aria-label="Table View"
-            >
-              <MdTableView />
-            </ActionIcon>
-          </Tooltip>
-        </ActionIcon.Group>
-      </span>
+        <Card p={"xs"}>
+          <ActionIcon.Group>
+            <Tooltip label="Tampilan Kamera">
+              <ActionIcon
+                variant={showTable ? "default" : "filled"}
+                size={"lg"}
+                onClick={() => setShowTable(false)}
+                aria-label="Camera"
+              >
+                <MdCamera />
+              </ActionIcon>
+            </Tooltip>
+            <Tooltip label="Tampilan Tabel">
+              <ActionIcon
+                variant={showTable ? "filled" : "default"}
+                size={"lg"}
+                onClick={() => setShowTable(true)}
+                aria-label="Table View"
+              >
+                <MdTableView />
+              </ActionIcon>
+            </Tooltip>
+          </ActionIcon.Group>
+        </Card>
+      </Flex>
       {showTable ? (
         <Table.ScrollContainer
           bd={"1px solid myColor.5"}
-          className="hover:resize aspect-video"
-          minWidth={720}
+          className="w-full aspect-video"
         >
           <ReportTable data={sampleDataCCTV} />
         </Table.ScrollContainer>
       ) : (
-        <ImageStreamViewer base64Data={stream.base64data} minW={720} />
+        <ImageStreamViewer base64Data={stream.base64data} minW={500} />
       )}
       <br />
-    </>
-  );
-};
-
-const Inspector = () => {
-  return (
-    <div className="flex flex-col p-2 gap-2">
-      <Text size="xl">Pengaturan</Text>
-      <RtspInput />
-    </div>
-  );
-};
-
-const RtspInput = () => {
-  return (
-    <>
-      <TextInput
-        label="RTSP URL"
-        placeholder="rtsp://urlname"
-        rightSection={
-          <ActionIcon variant="filled">
-            <MdOutlinePlayArrow />
-          </ActionIcon>
-        }
-      />
     </>
   );
 };
