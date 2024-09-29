71f3909b4972c803c5ad79516fb391935e7b9a57
Agung Adhinata <adhi0asta@gmail.com>
2024-09-29 12:29:58 +0800

    remove register

--- frontend/src/routes/register.tsx
+++
@@ -1,5 +0,0 @@
-import { RegisterPage } from "@/pages/RegisterPage"
-import {createFileRoute} from "@tanstack/react-router"
-export const Route = createFileRoute("/register")({
-    component: RegisterPage
-})
