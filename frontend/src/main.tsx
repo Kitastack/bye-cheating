6ac66a902061f7fccf3a70eb9e7f7eba31eef979
Agung Adhinata <adhi0asta@gmail.com>
2024-09-29 12:23:52 +0800

    cleanup

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -5,12 +5,10 @@ import "@fontsource-variable/karla";
 import "@/index.css";
 import "@mantine/core/styles.css";
 import "@mantine/notifications/styles.css";
-import i18n from "@/lib/i18n";
 import { MantineProvider } from "@mantine/core";
 import { StreamSocketProvider } from "./components/context/CameraSocketContext";
 import { ModalsProvider } from "@mantine/modals";
 import { Notifications } from "@mantine/notifications";
-import { I18nextProvider } from "react-i18next";
 import { appTheme } from "./theme";
 import { createRouter, RouterProvider } from "@tanstack/react-router";
 
@@ -28,15 +26,13 @@ const container = document.getElementById("root") as ReactDOM.Container;
 
 ReactDOM.createRoot(container).render(
   <React.StrictMode>
-    <I18nextProvider i18n={i18n}>
-      <MantineProvider theme={appTheme} defaultColorScheme="light">
-        <Notifications />
-        <ModalsProvider>
-          <StreamSocketProvider>
-            <RouterProvider router={router}  />
-          </StreamSocketProvider>
-        </ModalsProvider>
-      </MantineProvider>
-    </I18nextProvider>
+    <MantineProvider theme={appTheme} defaultColorScheme="light">
+      <Notifications />
+      <ModalsProvider>
+        <StreamSocketProvider>
+          <RouterProvider router={router} />
+        </StreamSocketProvider>
+      </ModalsProvider>
+    </MantineProvider>
   </React.StrictMode>
 );
